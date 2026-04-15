cf56d3279994fdaa389d9b2a5dae40e660555ca4
niranjana316 <niranjana.b2022@vitstudent.ac.in>
2026-04-15 20:21:32 +0530

    Update deadlock.py

--- deadlock.py
+++ deadlock.py
@@ -1,11 +1,27 @@
+import simpy
 import networkx as nx
+import random
 
-class WaitForGraph:
-    def __init__(self):
+class DistributedSystem:
+    def __init__(self, env, num_processes):
+        self.env = env
+        self.num_processes = num_processes
         self.graph = nx.DiGraph()
 
-    def add_edge(self, from_p, to_p):
-        self.graph.add_edge(from_p, to_p)
+    def process(self, pid):
+        yield self.env.timeout(random.randint(1, 3))
+
+        # randomly wait for another process
+        wait_for = random.randint(0, self.num_processes - 1)
+
+        if pid != wait_for:
+            self.graph.add_edge(f"P{pid}", f"P{wait_for}")
+
+    def run(self):
+        for i in range(self.num_processes):
+            self.env.process(self.process(i))
+
+        self.env.run()
 
     def detect_deadlock(self):
         try:
